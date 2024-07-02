f0900888e5bf66f41f632b3a08af0d870ba693ab
Milan Jovanovic <Mjovanovic3875@conestogac.on.ca>
2024-07-02 12:06:16 -0400

    organization page, manage organiation, organization otions, view contriutions modeled

--- app/org_home/page.tsx
+++ app/org_home/page.tsx
@@ -6,12 +6,12 @@ import React from 'react'
 const Org_Home = () => {
   return (
     <div className="container">
-      <p>Organization Home</p>
+      <p>Place Holder Organization Home</p>
       <button className="button" onClick={() => window.location.href = "/org_options"}>Organization Options</button>
       <br />
       <button className="button" onClick={() => window.location.href = "/project_home"}>View all Projects</button>
       <br />
-      <button className="button" onClick={() => window.location.href = "/org"}>View All Organizations</button>
+      <button className="buttonback" onClick={() => window.location.href = "/org"}>View All Organizations</button>
     </div>
   );
 };
