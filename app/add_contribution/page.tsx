bec8c1ee9d78aa9d9ebd9d1b4bf29e12f0d6484a
Raven So <Rso2794@conestogac.on.ca>
2024-07-04 19:48:27 -0400

    Added nav bar for all pages

--- app/add_contribution/page.tsx
+++ app/add_contribution/page.tsx
@@ -4,7 +4,7 @@ const Submit_Contribution = () => {
   return (
     <div>
       <div>
-        <p><a href="/org">Organizations</a> {"<"} <a href="/project_home">Project Home</a> {"<"} <a href="/project_options">Project Options</a></p>
+        <p><a href="/org">Organizations</a> {"<"} <a href="/org_home">Organization Home</a> {"<"} <a href="/project_home">Project Home</a> {"<"} <a href="/project_options">Project Options</a></p>
       </div>
       <div>
         <p>Submit Contribution</p>
