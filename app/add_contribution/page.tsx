9347d52a6066cbb090f77d2a930fd7d01942a35d
Raven So <Rso2794@conestogac.on.ca>
2024-07-03 21:05:54 -0400

    quick fix to 'add_contribution' title

--- app/add_contribution/page.tsx
+++ app/add_contribution/page.tsx
@@ -7,7 +7,7 @@ const Submit_Contribution = () => {
         <p><a href="/org">Organizations</a> {"<"} <a href="/project_home">Project Home</a> {"<"} <a href="/project_options">Project Options</a></p>
       </div>
       <div>
-        <p>Submit Contribution</p>
+        <h1>Submit Contribution</h1>
         <a href="/add_item"><button type="button">Add A Item</button></a>
         <br/><br/>
         <a href="/add_service"><button type="button">Add A Service</button></a>
