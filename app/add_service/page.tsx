983a03264a74eeb994a2820b79da5d65923f7479
Raven So <Rso2794@conestogac.on.ca>
2024-07-04 20:20:09 -0400

    added 'getDate' function to add_service

--- app/add_service/page.tsx
+++ app/add_service/page.tsx
@@ -1,5 +1,14 @@
 "use client";
-import React from 'react'
+import React, {useState} from 'react'
+
+function getDate()
+{
+  const today = new Date();
+  const month = today.getMonth()+1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${month}/${date}/${year}`;
+}
 
 const Add_Service = () => {
   return (
