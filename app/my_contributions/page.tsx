f3074524a4a314332b6533d02d9c16e82cb7d111
Raven So <Rso2794@conestogac.on.ca>
2024-07-18 23:27:17 -0400

    Contribution table changes

--- app/my_contributions/page.tsx
+++ app/my_contributions/page.tsx
@@ -2,9 +2,9 @@
 import React from 'react'
 
 const fakeContributions = [
-  {name: "service1", quantity: "1.5", cost: "0", type: "service"},
-  {name: "item1", quantity: "2", cost: "2500", type: "item"},
-  {name: "service2", quantity: "2.0", cost: "0", type: "service"}
+  {name: "service1", quantity: "1.5", worth: "20", type: "service", details: "test item 1"},
+  {name: "item1", quantity: "2", worth: "2500", type: "item", details: "test item 2"},
+  {name: "service2", quantity: "2.0", worth: "18", type: "service", details: "test item 3"}
 ]
 const my_contributions = () => {
   return (
@@ -17,34 +17,24 @@ const my_contributions = () => {
         <table style={{border: "2px solid forestgreen", width: "800px", height: "200px"}}>
           <tr>
             <th style={{borderBottom: "1px solid black"}}>Name</th>
-            <th style={{borderBottom: "1px solid black"}}>Hours/Quantity</th>
-            <th style={{borderBottom: "1px solid black"}}>Cost</th>
-            <th style={{borderBottom: "1px solid black"}}>Value</th>
             <th style={{borderBottom: "1px solid black"}}>Type</th>
+            <th style={{borderBottom: "1px solid black"}}>Hours/Items</th>
+            <th style={{borderBottom: "1px solid black"}}>Value Per</th>
+            <th style={{borderBottom: "1px solid black"}}>Total Value</th>
+            <th style={{borderBottom: "1px solid black"}}>Details</th>
           </tr>
           {fakeContributions.map((val, key)=>{
-            let value = parseFloat(val.quantity) * parseFloat(val.cost);
-            if(val.type == "Service"){
-              return (
-                <tr key={key}>
-                  <td style={{textAlign: "center"}}>{val.name}</td>
-                  <td style={{textAlign: "center"}}>{val.quantity}</td>
-                  <td style={{textAlign: "center"}}>{val.cost}</td>
-                  <td style={{textAlign: "center"}}>{value}</td>
-                  <td style={{textAlign: "center"}}>{val.type}</td>
-                </tr>
-              )
-            }else{
-              return (
-                <tr key={key}>
-                  <td style={{textAlign: "center"}}>{val.name}</td>
-                  <td style={{textAlign: "center"}}>{val.quantity} hrs</td>
-                  <td style={{textAlign: "center"}}>{val.cost}</td>
-                  <td style={{textAlign: "center"}}>{value}</td>
-                  <td style={{textAlign: "center"}}>{val.type}</td>
-                </tr>
-              )
-            }
+            let value = parseFloat(val.quantity) * parseFloat(val.worth);
+            return (
+              <tr key={key}>
+                <td style={{textAlign: "center"}}>{val.name}</td>
+                <td style={{textAlign: "center"}}>{val.quantity}</td>
+                <td style={{textAlign: "center"}}>$ {val.worth}</td>
+                <td style={{textAlign: "center"}}>{value}</td>
+                <td style={{textAlign: "center"}}>{val.type}</td>
+                <td style={{textAlign: "center"}}>{val.details}</td>
+              </tr>
+            )
           })}
         </table>
       </div>
