eec431e9c3c156a13ed2c6310d69410bc563be9b
Raven So <Rso2794@conestogac.on.ca>
2024-07-24 13:01:44 -0400

    fixing connector (broken)

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from './components/Layout';
 import './globals.css';
+import ApolloClientProvider from './apollo-client';
+
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <Layout>
-                    {children}
-                </Layout>
+                <ApolloClientProvider>
+                    <Layout>
+                        {children}
+                    </Layout>
+                </ApolloClientProvider>
             </body>
         </html>
     );
--- app/view_contributions/page.tsx
+++ app/view_contributions/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+"use client";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import Layout from "../components/Layout";
 
 const fakeContributions = [
-  {name: "service1", quantity: "1.5", worth: "20", type: "service", details: "test item 1"},
-  {name: "item1", quantity: "2", worth: "2500", type: "item", details: "test item 2"},
-  {name: "service2", quantity: "2.0", worth: "18", type: "service", details: "test item 3"}
+  {name: "service1", type: "service", quantity: "1.5", worth: "20", details: "test item 1"},
+  {name: "item1", type: "item", quantity: "2", worth: "2500", details: "test item 2"},
+  {name: "service2", type: "service", quantity: "2.0", worth: "18", details: "test item 3"}
 ]
 
 const my_contributions = () => {
@@ -26,10 +29,10 @@ const my_contributions = () => {
             return (
               <tr key={key}>
                 <td style={{textAlign: "center"}}>{val.name}</td>
-                <td style={{textAlign: "center"}}>{val.quantity}</td>
+                <td style={{textAlign: "center"}}>{val.type}</td>
                 <td style={{textAlign: "center"}}>$ {val.worth}</td>
                 <td style={{textAlign: "center"}}>{value}</td>
-                <td style={{textAlign: "center"}}>{val.type}</td>
+                <td style={{textAlign: "center"}}>{val.quantity}</td>
                 <td style={{textAlign: "center"}}>{val.details}</td>
               </tr>
             )
