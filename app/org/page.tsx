11f3cc361f705d213da09a9ee379a0d203fdbbb7
Torin Sparks <tsparks9038@conestogac.on.ca>
2024-07-24 14:12:46 -0400

    Integrated partner org page with backend.

--- app/org/page.tsx
+++ app/org/page.tsx
@@ -1,11 +1,62 @@
 "use client";
+
 import { gql, useQuery } from "@apollo/client";
 
+type Contributor = {
+  id: string;
+  userId: string;
+  partnerOrgId: string;
+  researchOrgId: string;
+  hourlyRate: number;
+  benRatePer: number;
+};
+
+type PartnerOrg = {
+  id: string;
+  name: string;
+  admins: string[];
+  contributors: Contributor[];
+};
+
+export const GET_PARTNER_ORGS_FOR_USER = gql`
+  query getAllOrgsForUser($userId: ID!) {
+    getAllOrgsForUser(userId: $userId) {
+      admins
+      contributors {
+        benRatePer
+        hourlyRate
+        id
+        partnerOrgId
+        researchOrgId
+        userId
+      }
+      id
+      name
+    }
+  }
+`;
+
 const SelectOrganization = () => {
+  const { loading, error, data } = useQuery(GET_PARTNER_ORGS_FOR_USER, {
+    variables: { userId: "402a8052-9c4d-496e-bd17-d25f3d0c2bf7" },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error: {error.message}</p>;
+  }
+
+  console.log(data);
+
   return (
     <div className="container">
       <h1>Select Organization</h1>
-      <button className="button" onClick={() => window.location.href = "/org_home"}>Place Holder Org</button>
+      {data?.getAllOrgsForUser?.map((partnerOrg: PartnerOrg) => (
+        <button key={partnerOrg.id} className="button" onClick={() => window.location.href = "/org_home"}>
+          {partnerOrg.name}
+        </button>
+      ))}
     </div>
   );
 };
