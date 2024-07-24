d86b19f81c84021ee85b7987233188b6c547746d
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2024-07-24 13:41:04 -0400

    fixed view query page

--- app/my_contributions/page.tsx
+++ app/my_contributions/page.tsx
@@ -1,32 +1,32 @@
 "use client";
 import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 
 type HourContribution = {
-  hours: Number;
-  hourlyRate: Number;
-  benRatePer: Number;
-}
+  hours: number;
+  hourlyRate: number;
+  benRatePer: number;
+};
 
 type OtherContribution = {
-  itemName: String;
-  value: Number;
-  quantity: Number;
-}
+  itemName: string;
+  value: number;
+  items: number;
+};
 
 type Contribution = {
-  id: String;
-  contributorId: String;
-  date: String;
-  details: String;
+  id: string;
+  contributorId: string;
+  date: string;
+  details: string;
 
   hourContribution: HourContribution;
   otherContribution: OtherContribution;
-}
+};
 
 const GET_CONTRIBUTIONS_FOR_USER = gql`
-  query UserContributions($userId:ID!) {
+  query UserContributions($userId: ID!) {
     getAllContributionsForUser(userId: $userId) {
       id
       date
@@ -42,7 +42,7 @@ const GET_CONTRIBUTIONS_FOR_USER = gql`
         items
       }
     }
-  } 
+  }
 `;
 
 // const fakeContributions = [
@@ -50,46 +50,96 @@ const GET_CONTRIBUTIONS_FOR_USER = gql`
 //   {name: "item1", type: "item", quantity: "2", worth: "2500", details: "test item 2"},
 //   {name: "service2", type: "service", quantity: "2.0", worth: "18", details: "test item 3"}
 // ]
-export default function my_contributions() {
-  
-  useEffect(()=>{const { loading, error, data } = useQuery(GET_CONTRIBUTIONS_FOR_USER, {variables: {userId:"24a7cf8c-feca-4863-87e8-1952a18a6973"},}); },[]);
-  
-  console.log(`${data}`);
+export default function MyContributions() {
+  const { loading, error, data } = useQuery(GET_CONTRIBUTIONS_FOR_USER, {
+    variables: { userId: "24a7cf8c-feca-4863-87e8-1952a18a6973" },
+  });
+  console.log(data?.getAllContributionsForUser);
   // if (loading) return <p>Loading...</p>;
   // if (error) return <p>Error: </p>;
 
   return (
     <div>
-      {data}
-      {/* <div>
-        <p><a href="/org">Organizations</a> {"<"} <a href="/org_home">Organization Home</a> {"<"} <a href="/project_home">Project Home</a> {"<"} <a href="/project_options">Project Options</a></p>
+      <div>
+        <p>
+          <a href="/org">Organizations</a> {"<"}{" "}
+          <a href="/org_home">Organization Home</a> {"<"}{" "}
+          <a href="/project_home">Project Home</a> {"<"}{" "}
+          <a href="/project_options">Project Options</a>
+        </p>
       </div>
       <h1>My Contributions</h1>
       <div>
-        <table style={{border: "2px solid forestgreen", width: "800px", height: "200px"}}>
+        <table
+          style={{
+            border: "2px solid forestgreen",
+            width: "800px",
+            height: "200px",
+          }}
+        >
           <tr>
-            <th style={{borderBottom: "1px solid black"}}>Name</th>
-            <th style={{borderBottom: "1px solid black"}}>Type</th>
-            <th style={{borderBottom: "1px solid black"}}>Hours/Items</th>
-            <th style={{borderBottom: "1px solid black"}}>Value Per</th>
-            <th style={{borderBottom: "1px solid black"}}>Total Value</th>
-            <th style={{borderBottom: "1px solid black"}}>Details</th>
+            <th style={{ borderBottom: "1px solid black" }}>Contribution ID</th>
+            <th style={{ borderBottom: "1px solid black" }}>Date</th>
+            <th style={{ borderBottom: "1px solid black" }}>Details</th>
+            <th style={{ borderBottom: "1px solid black" }}>Type</th>
+
+            <th style={{ borderBottom: "1px solid black" }}>Hours/Items</th>
+            <th style={{ borderBottom: "1px solid black" }}>Value Per</th>
+            <th style={{ borderBottom: "1px solid black" }}>Total Value</th>
           </tr>
-          {data?.map((val:Contribution)=>{
-            let value = parseFloat(val.quantity) * parseFloat(val.worth);
-            return (
-              <tr>
-                <td style={{textAlign: "center"}}>{val.id}</td>
-                <td style={{textAlign: "center"}}>{val.type}</td>
+          {data?.getAllContributionsForUser?.map(
+            (val: Contribution, key: number) => {
+              const date = new Date(val.date).toDateString();
+              return (
+                <tr key={key}>
+                  <td style={{ textAlign: "center" }}>{val.id}</td>
+                  <td style={{ textAlign: "center" }}>{date}</td>
+                  <td style={{ textAlign: "center" }}>{val.details}</td>
+                  {!!val.hourContribution && (
+                    <>
+                      <td style={{ textAlign: "center" }}>{"Hourly"}</td>
+
+                      <td
+                        style={{ textAlign: "center" }}
+                      >{`${val?.hourContribution?.hours}`}</td>
+                      <td
+                        style={{ textAlign: "center" }}
+                      >{`${val?.hourContribution?.hourlyRate}`}</td>
+                      <td style={{ textAlign: "center" }}>{`${
+                        val?.hourContribution?.hourlyRate *
+                        val?.hourContribution?.hours
+                      }`}</td>
+                    </>
+                  )}
+                  {!!val.otherContribution && (
+                    <>
+                      <td style={{ textAlign: "center" }}>
+                        {val.otherContribution.itemName}
+                      </td>
+
+                      <td
+                        style={{ textAlign: "center" }}
+                      >{`${val?.otherContribution?.items}`}</td>
+                      <td
+                        style={{ textAlign: "center" }}
+                      >{`${val?.otherContribution?.value}`}</td>
+                      <td style={{ textAlign: "center" }}>{`${
+                        val?.otherContribution?.value *
+                        val?.otherContribution?.items
+                      }`}</td>
+                    </>
+                  )}
+                  {/*                   
+                  <td style={{textAlign: "center"}}>{val.type}</td>
                 <td style={{textAlign: "center"}}>$ {val.worth}</td>
                 <td style={{textAlign: "center"}}>{value}</td>
-                <td style={{textAlign: "center"}}>{val.quantity}</td>
-                <td style={{textAlign: "center"}}>{val.details}</td>
-              </tr>
-            )
-          })}
+                <td style={{textAlign: "center"}}>{val.quantity}</td> */}
+                </tr>
+              );
+            }
+          )}
         </table>
-      </div> */}
+      </div>
     </div>
-  )
+  );
 }
--- app/view_contributions/page.tsx
+++ app/view_contributions/page.tsx
@@ -4,44 +4,71 @@ import { useEffect } from "react";
 import Layout from "../components/Layout";
 
 const fakeContributions = [
-  {name: "service1", type: "service", quantity: "1.5", worth: "20", details: "test item 1"},
-  {name: "item1", type: "item", quantity: "2", worth: "2500", details: "test item 2"},
-  {name: "service2", type: "service", quantity: "2.0", worth: "18", details: "test item 3"}
-]
+  {
+    name: "service1",
+    type: "service",
+    quantity: "1.5",
+    worth: "20",
+    details: "test item 1",
+  },
+  {
+    name: "item1",
+    type: "item",
+    quantity: "2",
+    worth: "2500",
+    details: "test item 2",
+  },
+  {
+    name: "service2",
+    type: "service",
+    quantity: "2.0",
+    worth: "18",
+    details: "test item 3",
+  },
+];
 
-const my_contributions = () => {
+const MyContributions = () => {
   return (
     <div>
-      <p><a href="/org">Organizations</a> {"<"} <a href="/org_home">Organization Home</a> {"<"} <a href="/org_options">Organization Options</a></p>
+      <p>
+        <a href="/org">Organizations</a> {"<"}{" "}
+        <a href="/org_home">Organization Home</a> {"<"}{" "}
+        <a href="/org_options">Organization Options</a>
+      </p>
       <h1>All Contributions</h1>
       <div>
-        <table style={{border: "2px solid forestgreen", width: "800px", height: "200px"}}>
+        <table
+          style={{
+            border: "2px solid forestgreen",
+            width: "800px",
+            height: "200px",
+          }}
+        >
           <tr>
-            <th style={{borderBottom: "1px solid black"}}>Name</th>
-            <th style={{borderBottom: "1px solid black"}}>Type</th>
-            <th style={{borderBottom: "1px solid black"}}>Hours/Items</th>
-            <th style={{borderBottom: "1px solid black"}}>Value Per</th>
-            <th style={{borderBottom: "1px solid black"}}>Total Value</th>
-            <th style={{borderBottom: "1px solid black"}}>Details</th>
+            <th style={{ borderBottom: "1px solid black" }}>Name</th>
+            <th style={{ borderBottom: "1px solid black" }}>Type</th>
+            <th style={{ borderBottom: "1px solid black" }}>Hours/Items</th>
+            <th style={{ borderBottom: "1px solid black" }}>Value Per</th>
+            <th style={{ borderBottom: "1px solid black" }}>Total Value</th>
+            <th style={{ borderBottom: "1px solid black" }}>Details</th>
           </tr>
-          {fakeContributions.map((val, key)=>{
+          {fakeContributions.map((val, key) => {
             let value = parseFloat(val.quantity) * parseFloat(val.worth);
             return (
               <tr key={key}>
-                <td style={{textAlign: "center"}}>{val.name}</td>
-                <td style={{textAlign: "center"}}>{val.type}</td>
-                <td style={{textAlign: "center"}}>$ {val.worth}</td>
-                <td style={{textAlign: "center"}}>{value}</td>
-                <td style={{textAlign: "center"}}>{val.quantity}</td>
-                <td style={{textAlign: "center"}}>{val.details}</td>
+                <td style={{ textAlign: "center" }}>{val.name}</td>
+                <td style={{ textAlign: "center" }}>{val.type}</td>
+                <td style={{ textAlign: "center" }}>$ {val.worth}</td>
+                <td style={{ textAlign: "center" }}>{value}</td>
+                <td style={{ textAlign: "center" }}>{val.quantity}</td>
+                <td style={{ textAlign: "center" }}>{val.details}</td>
               </tr>
-            )
+            );
           })}
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default my_contributions
+export default MyContributions;
